aba06b0bd89be7800e1cb2d11a717909e21fd381
Ujjwal <User@Ujjwals-MacBook-Pro.local>
2020-04-11 18:52:28 -0400

    changed

--- script/app.js
+++ script/app.js
@@ -1,11 +1,18 @@
 (() => {
+	
+	
+	
+	var musicSrc = ["music/bestOfBest.mp3", "music/eventually.mp3", "music/long.mp3", "music/ringtone.mp3", "music/when.mp3"];
+	var myMusic = new Audio();
+	var soundCounter = 0;
+	
 	// set up the puzzle pieces and boards
 	const puzzleButtons = document.querySelectorAll('#buttonHolder img'),
-				puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
-				dropZones = document.querySelectorAll('.drop-zone'),
-				gameBoard = document.querySelector('.puzzle-board');
+		puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
+		dropZones = document.querySelectorAll('.drop-zone'),
+		gameBoard = document.querySelector('.puzzle-board');
 
-	const pieceNames = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
+	const pieceNames = ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'];
 
 	function changeImageSet() {
 		// change all the image elements on the page -> draggable image sources
@@ -14,41 +21,66 @@
 			puzzlePieces[index].src = `images/${piece + this.dataset.puzzleref}.jpg`;
 			puzzlePieces[index].id = `${piece + this.dataset.puzzleref}`;
 		});
-
+		console.log(puzzlePieces[index].src);
+		
 
 		// and set the drop zone background image based on the puzzle the user selects
 		gameBoard.style.backgroundImage = `url(images/backGround${this.dataset.puzzleref}.jpg)`;
+
+
 		// debugger;
 	}
-  function allowDrag(event) {
-		console.log('started dragging an image')
+	function allowDrag(event) {
+		console.log('started dragging an image');
 
-		event.dataTransfer.setData("text/plain", this.id);
-  }
+		event.dataTransfer.setData('text/plain', this.id);
+	}
 	function allowDragOver(event) {
 		event.preventDefault();
 		console.log('dragged over a drop zones');
-
 	}
 	function allowDrop(event) {
 		// event.preventDefault();
-		console.log('dropped on a drop zone')
+		console.log('dropped on a drop zone');
+
+		
 
 		// go and get the dragged element's ID frm the data transfer object
-		let currentImage = event.dataTransfer.getData("text/plain");
+		let currentImage = event.dataTransfer.getData('text/plain');
+		console.log(currentImage);
 
+		elem = document.createElement('img');
+		elem.setAttribute('src', currentImage);
+		elem.setAttribute('height', '100%');
+		elem.setAttribute('width', 'auto');
 		// add that image to whatever drop zone  we're dropping our image on
-		event.target.appendChild(document.querySelector(`#${currentImage}`));
-  }
+		event.target.appendChild(elem);
+		// event.target.appendChild(document.querySelector(`${currentImage}`));
+		
+		if(soundCounter < musicSrc.length){
+			myMusic.pause();
+			myMusic = new Audio(musicSrc[soundCounter]);
+			myMusic.loop = true;
+			myMusic.play();
+			soundCounter++;
+			if(soundCounter == musicSrc.length){
+				soundCounter = 0;
+			}
+		}
+	}
 
 	//add event handling here -> how is the user going to use our app?
 	// what triggers do we need?
 	// click on the bottom buttons to change the puzzle image we're working with
-	puzzleButtons.forEach(button => button.addEventListener('click', changeImageSet));
+	puzzleButtons.forEach((button) =>
+		button.addEventListener('click', changeImageSet)
+	);
 
-	puzzlePieces.forEach(piece => piece.addEventListener('dragstart', allowDrag));
+	puzzlePieces.forEach((piece) =>
+		piece.addEventListener('dragstart', allowDrag)
+	);
 
-	dropZones.forEach(zone => {
+	dropZones.forEach((zone) => {
 		zone.addEventListener('dragover', allowDragOver);
 		zone.addEventListener('drop', allowDrop);
 	});
@@ -58,15 +90,15 @@
 	changeImageSet.call(puzzleButtons[0]);
 })();
 function allowDrop(ev) {
-  ev.preventDefault();
+	ev.preventDefault();
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+	ev.dataTransfer.setData('text', ev.target.id);
 }
 
 function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData('text');
+	ev.target.appendChild(document.getElementById(data));
 }
